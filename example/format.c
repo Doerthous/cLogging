cd5a2a7f59acbac9c10e669d461405342e9e5f89
doerthous <doerthous@gmail.com>
2022-03-06 16:31:03 +0800

    test: update example

--- example/format.c
+++ example/format.c
@@ -4,6 +4,12 @@
 #define LOGGING_LOG_TIME
 #define LOGGING_LOG_DATETIME
 #define LOGGING_LOG_FUNCTION
+
+#define LOGGING_DEBUG_FLAG "[DEBUG]"
+#define LOGGING_INFO_FLAG "[INFO]"
+#define LOGGING_WARN_FLAG "[WARNING]"
+#define LOGGING_ERROR_FLAG "[ERROR]"
+
 #include "Logging.h"
 
 int main()
